834899d90c0dab6b1e0351cb0a7c77870310ae03
MahadAsif-2443 <148737286+MahadAsif-2443@users.noreply.github.com>
2024-09-18 17:54:48 +0000

    first hello world program

---
+++ 101/hello.cpp
@@ -0,0 +1,9 @@
+// we add c++ into dev container -- use this command for c "gcc 101/hello.c -o hello and then run executable file as new command ./hello" and use this command for cpp "g++ 101/hello.cpp -o hello and then run executable file as new command ./hello" --
+
+#include <iostream>
+
+int main() {
+    // Print the message to the console
+    std::cout << "Hello, World!" << std::endl;
+    return 0;  // Indicate that the program ended successfully
+}
